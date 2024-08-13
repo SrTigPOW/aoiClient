2abfec481983c83f6cf0320d7764bd4a2534395c
SrTig <162766265+SrTigPOW@users.noreply.github.com>
2024-08-13 01:59:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 
 const token = process.env.DISCORD_TOKEN
 
+// In this case, the bot is configured to use only one shard (totalShards: 1).
+// You can change this value according to your bot's needs.
 const sharder = new ClientShard("./aoiClient.js", {
     token: token,
     totalShards: 1
