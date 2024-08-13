fcf50f0cd9e8ae88a7d8491613417ad56537fea4
SrTig <162766265+SrTigPOW@users.noreply.github.com>
2024-08-13 02:06:31 -0300

    Update clientVariables.js

--- src/handler/clientVariables.js
+++ src/handler/clientVariables.js
@@ -1,3 +1,5 @@
+// Loads the environment variables defined in the `.env` file using the `dotenv` package.
+// If you're not familiar with this, it's advisable not to modify it.
 require('dotenv').config();
 const prefix = process.env.PREFIX
 
